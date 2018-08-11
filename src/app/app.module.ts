0ca51e1798589e77019bbce20697bd11ce413b95
mnowak <mateusz.nowak@intenso.pl>
2018-08-11 14:09:40 +0200

    Add mock of authentication.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
@@ -16,6 +16,7 @@ import {FlexLayoutModule} from '@angular/flex-layout';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import {StopTrainingComponent} from './training/current-training/stop-training.component';
+import {AuthService} from './auth/auth.service';
 
 
 @NgModule({
@@ -35,12 +36,13 @@ import {StopTrainingComponent} from './training/current-training/stop-training.c
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MaterialModule,
     AppRoutingModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent],
   entryComponents: [StopTrainingComponent]
 })
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,38 @@
+import {User} from './user.model';
+import {AuthData} from './auth-data.model';
+import {Guid} from './guid';
+import {Subject} from 'rxjs';
 
 export class AuthService {
+  authChange = new Subject<boolean>();
+  private user: User;
 
+  registerUser(authData: AuthData) {
+    this.user = {
+      email: authData.email,
+      userId: Guid.newGuid()
+    };
+    this.authChange.next(true);
+  }
+
+  login(authData: AuthData) {
+    this.user = {
+      email: authData.email,
+      userId: Guid.newGuid()
+    };
+    this.authChange.next(true);
+  }
+
+  logout() {
+    this.user = null;
+    this.authChange.next(false);
+  }
+
+  getUser() {
+    return {...this.user};
+  }
+
+  isAuth() {
+    return this.user != null;
+  }
 }
